3606673f9351d6754365737cf086207e942cdf6e
hieuhuynh12 <66456765+hieuhuynh12@users.noreply.github.com>
2020-06-27 18:44:25 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,6 @@
 <?php
 define('DB_SERVER','us-cdbr-east-02.cleardb.com');
-define('DB_USER','bd8d2dc12c12a8
-');
+define('DB_USER','bd8d2dc12c12a8');
 define('DB_PASS' ,'44876425');
 define('DB_NAME', 'heroku_02cc6207daf1a08');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
