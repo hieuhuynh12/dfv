92d4814e808699d48012368d1ce4a7a7edfdf24e
hieuhuynh12 <66456765+hieuhuynh12@users.noreply.github.com>
2020-06-27 18:34:01 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','us-cdbr-east-02.cleardb.com');
-define('DB_USER','bb89690bd8d2dc12c12a8493efe0');
-define('DB_PASS' ,'44876425');
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
 define('DB_NAME', 'shopping');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
