6f503cd7fc1f44fb596de7d26fb49d18ad0a3129
hieuhuynh12 <66456765+hieuhuynh12@users.noreply.github.com>
2020-06-27 18:43:32 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,9 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'shopping');
+define('DB_SERVER','us-cdbr-east-02.cleardb.com');
+define('DB_USER','bd8d2dc12c12a8
+');
+define('DB_PASS' ,'44876425');
+define('DB_NAME', 'heroku_02cc6207daf1a08');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
